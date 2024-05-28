0340b1a42060d0f230f622d45da0be89f367a132
Joseph <josephshonko99@gmail.com>
2024-05-28 15:23:28 +0300

    commiting instance methods

--- lib/dog.py
+++ lib/dog.py
@@ -1,7 +1,17 @@
 #!/usr/bin/env python3
 
+
+# dog.py
+
 class Dog:
+    def bark(self):
+        print("Woof!")
+
+    def sit(self):
+        print("The dog is sitting.")
+
+
     # Class body goes here
 
     #Instance method definition
-    pass
+    # pass
--- lib/person.py
+++ lib/person.py
@@ -1,6 +1,14 @@
 #!/usr/bin/env python3
 
 class Person:
+
+    # person.py
+    def talk(self):
+        print("Hello World!")
+
+    def walk(self):
+        print("The person is walking.")
+
     # Class body goes here
 
     #Instance method definition
